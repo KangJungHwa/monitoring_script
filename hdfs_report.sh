5898d1dab100d5dce8e8bdb8ea4cefb18e035e1f
JungHwa Kang <pacino.kang@gmail.com>
2019-02-28 17:53:40 +0900

    Update hdfs_report.sh

--- hdfs_report.sh
+++ hdfs_report.sh
@@ -1,19 +1,24 @@
 echo "################################"
-echo "Grand total"
+echo "Usage Grand Total"
 echo "################################"
 hdfs dfs -du -s -h /
 echo " "
 
 echo "################################"
-echo "Total by directory"
+echo "Usage Total By Directory"
 echo "################################"
 directory=$(hdfs dfs -ls / | awk ' NR == 1 {next} {printf $8 "\n"}')
 hdfs dfs -du -s -h $directory
 echo " "
 
 echo "################################"
-echo "Total by user"
+echo "Usage Total By User"
 echo "################################"
 
 user=$(hdfs dfs -ls /user | awk ' NR == 1 {next} {printf $8 "\n"}')
 hdfs dfs -du -s -h $user
+echo " "
+
+echo "################################"
+echo " HDFS Health Check Report"
+echo "################################"
